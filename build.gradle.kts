6e41d70d5d91ed2b15a45adcd4c7e8945d097906
Javed <javeddeng@outlook.com>
2026-03-18 23:13:26 +0800

    🚑️ 修复构建版本问题

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,23 +14,26 @@ repositories {
     }
 }
 
-java {
-    sourceCompatibility = JavaVersion.VERSION_21
-    targetCompatibility = JavaVersion.VERSION_21
-}
-
 dependencies {
     implementation("com.google.code.gson:gson:2.10.1")
-
-    intellijPlatform {
-        intellijIdeaCommunity("2024.2")
-        bundledPlugin("org.jetbrains.plugins.terminal")
-        testFramework(org.jetbrains.intellij.platform.gradle.TestFrameworkType.Platform)
-    }
-
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+
+    intellijPlatform {
+        intellijIdea("2024.2")
+        bundledPlugin("org.jetbrains.plugins.terminal")
+        testFramework(org.jetbrains.intellij.platform.gradle.TestFrameworkType.Platform)
+
+    }
+}
+
+intellijPlatform {
+    pluginConfiguration {
+        ideaVersion {
+            sinceBuild = "242"
+        }
+    }
 }
 
 tasks {
